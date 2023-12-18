b8f53912ec3f68928e6ba68ae7d1ffa07faf0385
JanaFadl <jfadl390@gmail.com>
2023-12-18 23:50:38 +0200

    sum function

--- practical.cpp
+++ practical.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
 using namespace std;
-
+int sum(int a, int b);
 int main() {
 
 
 	cout << "Hello \n";
-
+	cout << sum(2, 3);
 
 
 
 	return 0;
+}
+int sum(int a, int b) {
+	return a + b;
 }
